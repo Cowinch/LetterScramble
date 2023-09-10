56e0b2ae0eaa0ea07def6ddf277b841e45453580
Cole Winchester <cowinch@gmail.com>
2023-09-09 19:03:37 -0500

    improved readability

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 const letters = "ABCDEFGHIOJKLMNOPQRSTUVWXYZ"
+let originalLetter = (word, index) => word.dataset.value[index]
+let randomLetter = () => letters[Math.floor(Math.random() * 26)]
 
-function randomLetter() {
-    return letters[Math.floor(Math.random() * 26)]
-}
-
-function originalLetter(word, index){
-    return word.dataset.value[index]
-}
-
-document.querySelector('h1').onmouseover = event => {
+// Word scrambler
+document.querySelector('.scramble').onmouseover = event => {
     let word = event.target
     let iterations = 0
     const wordScrambleInterval = setInterval(() => {
         word.innerText = word.innerText.split('').map((letter, index) => {
             if (index < iterations) return originalLetter(word, index)
             else return randomLetter()
-        })
-            .join('')
+        }).join('')
         if (iterations >= word.dataset.value.length) clearInterval(wordScrambleInterval)
         iterations += 1 / 4
     }, 30)
